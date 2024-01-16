6abbc3a6f6990db9eb8a788db82ab170a1063bb8
princisgh <matteo.princisgh@studenti.unimi.it>
2024-01-16 19:25:41 +0100

    minor refactoring + added requirement

--- spawnContainers.py
+++ spawnContainers.py
@@ -1,4 +1,3 @@
-import sys
 import subprocess
 import os
 from concurrent.futures import ThreadPoolExecutor
@@ -16,24 +15,22 @@ def spawn_container(mapName: str, i, bar):
     p.wait()
 
 
-
 def main(workers: int):
-        pool = ThreadPoolExecutor(max_workers=workers) 
-        try:
-            with progressbar.ProgressBar(max_value=len(os.listdir("worlds/"))) as bar:
-                futures = []
-                for i, name in enumerate(os.listdir("worlds/")):
-                    if name.endswith(".world"):
-                        futures.append(pool.submit(spawn_container, name, i, bar))
-                pool.shutdown(wait=True)
-        except:
-            pool.shutdown(wait=False)
-            subprocess.Popen("docker kill $(docker ps -q)", shell=True)
-            return
+    pool = ThreadPoolExecutor(max_workers=workers)
+    try:
+        with progressbar.ProgressBar(max_value=len(os.listdir("worlds/")) - 2) as bar:
+            futures = []
+            for i, name in enumerate(os.listdir("worlds/")):
+                if name.endswith(".world"):
+                    futures.append(pool.submit(spawn_container, name, i, bar))
+            pool.shutdown(wait=True)
+    except:
+        pool.shutdown(wait=False)
+        subprocess.Popen("docker kill $(docker ps -q)", shell=True)
+        return
 
 
 if __name__ == "__main__":
     n = input("Insert number of workers to spawn:  ")
     main(int(n))
     print("All workers finished")
-
